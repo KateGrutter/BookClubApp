07c604c7d243bb0fec67685c728a49403aab5bf6
Maggie-Wernet <119549545+Maggie-Wernet@users.noreply.github.com>
2023-06-19 15:44:38 -0400

    header code update. Still not working 100%

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,35 +6,31 @@ import Bookmark from "../../assets/bookmark.svg";
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
+    <nav className="header">
+      <div className="header-logo">
         <Link to="/">
           <img src={Bookmark} alt="bookmark logo" />
         </Link>
       </div>
-      <div className={`navbar-menu ${isMenuOpen ? "active" : ""}`}>
-        <ul className="navbar-links">
+      <div>
+        <input className="side-menu" type="checkbox" id="side-menu" />
+        <label className="hamb" htmlFor="side-menu">
+          <span className="hamb-line"></span>
+        </label>
+      </div>
+      <div className="nav">
+        <ul className="menu">
           <li>
-            <Link to="/feed" onClick={toggleMenu}>
-              Home
-            </Link>
+            <Link to="/feed">Home</Link>
           </li>
           <li>
-            <Link to="/profile" onClick={toggleMenu}>
-              Profile
-            </Link>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/home">Logout</Link>
           </li>
         </ul>
-      </div>
-      <div className="navbar-toggle" onClick={toggleMenu}>
-        <span className="navbar-icon"></span>
-        <span className="navbar-icon"></span>
-        <span className="navbar-icon"></span>
       </div>
     </nav>
   );
