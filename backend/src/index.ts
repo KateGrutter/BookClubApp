ed90a4342ad6b4eb95f03072ac94e13642c2d6f1
Joan Wolf <joanewolf@gmail.com>
2023-06-19 19:43:10 -0400

    added routes to the back end

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/", userRouter);
 app.use("/", postRouter);
 
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
