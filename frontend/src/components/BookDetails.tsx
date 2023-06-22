c718223e3853126530078004400fa451265536fd
Maggie-Wernet <119549545+Maggie-Wernet@users.noreply.github.com>
2023-06-21 21:27:32 -0400

    updated my code to where Joan and Kate are

--- frontend/src/components/BookDetails.tsx
+++ frontend/src/components/BookDetails.tsx
@@ -1,27 +1,26 @@
 //this will have the books to map out when the user searches for a book
 
-import { useEffect, useState } from "react"
-import { bookDetails } from "../services/BookService";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { bookDetails, getBook } from "../services/BookService";
 
-// import { Book } from "../models/Book";
+import { Book } from "../models/Book";
 
-export function BookDetails(props: { onClose: () => void, id: string|undefined}) {
-    
-    const [details, setDetails] = useState<any>()
+export function BookDetails(props: {
+  onClose: () => void;
+  id: string | undefined;
+}) {
+  const [details, setDetails] = useState<any>();
+  const bookKey = useParams().key;
 
-   
-
-    useEffect(() => {
-        if (props.id !== undefined) {
-        bookDetails(props.id).then(data => setDetails(data))
-    }
-    }, [props.id]);
-// ! means it will not equal null
-    return(
-        <div>
-            {details?.title}
-            <button onClick={() => props.onClose()}>Cancel</button>
-            </div>
-        
-    )
+  useEffect(() => {
+    bookDetails(bookKey!).then((data) => setDetails(data));
+  }, [bookKey]);
+  // ! means it will not equal null
+  return (
+    <div>
+      {details?.title}
+      <button onClick={() => props.onClose()}>Cancel</button>
+    </div>
+  );
 }
