427befcbb46c9669d2eaad6e6232d198fd1c5e8a
Joan Wolf <joanewolf@gmail.com>
2023-06-22 19:45:11 -0400

    moved memberlist to its own component so as not to have to rename file in vscode

---
+++ frontend/src/components/MemberList.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { User } from "../models/User"
+import { getUsers } from "../services/ExpressService"
+import { Link } from "react-router-dom"
+
+export function MemberList() {
+
+const [users, setUsers] = useState<any>([])
+
+useEffect(() => {
+getUsers().then(data => setUsers(data))
+}, [])
+
+return (
+    <div id="member-list">
+        <h3>Members</h3>
+        {
+            users.map((user: User) => <ul>
+                <li key={user._id}><Link to={``}>Name: {user.firstName} {user.lastName}</Link></li>
+                </ul>)
+        }
+    </div>
+)
+}
