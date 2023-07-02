17da4bec2010f88d5e9d48db93b4924782552511
Kate Grutter <kategrutter@gmail.com>
2023-07-02 18:59:55 -0400

    the header menu bug fixed

--- frontend/src/components/PersonalProfile.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "../AuthContext";
-
-const {currentUser} = useContext(AuthContext);
-
-<p>UID: {currentUser?.uid}</p>;
-<p>Display name: {currentUser?.displayName}</p>;
-<p>Email: {currentUser?.email}</p>;
